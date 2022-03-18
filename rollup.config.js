30629ac4da7046f1140ed7807f91f7cff16634a9
feng <908087098@qq.com>
2022-03-18 15:50:23 +0800

    调整构建流程

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
+import pkg from './package.json';
 
 export default [
     {
         input: 'src/index.ts',
         output: {
-            file: './dist/index.js',
-            name: 'watcher',
+            file: pkg.bundle,
+            name: pkg.bundleName,
             format: 'umd',
             sourcemap: true
         },
@@ -17,10 +18,10 @@ export default [
     {
         input: 'src/index.ts',
         output: {
-            file: './dist/index.d.ts',
-            name: 'watcher',
+            file: pkg['bundle:types'],
+            name: pkg.bundleName,
             format: 'es',
-            footer: 'export as namespace watcher;'
+            footer: `export as namespace ${pkg.bundleName};`
         },
         plugins: [
             typescript({ clean: true, tsconfigOverride: { compilerOptions: { module: 'ES2015', declaration: false, declarationMap: false } } }),
