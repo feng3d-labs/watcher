8ca155014cf13c28621ec49356349a1ba2ff9ecf
feng <908087098@qq.com>
2025-03-08 00:58:49 +0800

    test(watcher): 测试 watchobject 对象深层次属性变更和赋值为 null 的情况
    - 添加了两个测试用例，分别验证深层次子对象和子对象赋值为 null 的情况
    - 确保 watcher.watchobject 能正确监听并处理这些变更
    - 验证 unwatchobject 方法在移除监听时的行为是否正确

--- test/watchobject.spec.ts
+++ test/watchobject.spec.ts
@@ -4,6 +4,124 @@ import { assert, describe, it } from 'vitest';
 
 describe('watcher.watchobject transform', () =>
 {
+    it('测试监听很深的子对象被赋值为null是否正常', () =>
+    {
+        const a = { a1: { a2: { a3: { a4: { a5: { a6: { a7: { a8: { a9: { a10: { a11: { a12: { a13: { a14: { a15: { a16: { a17: { a18: { a19: { a20: { a21: { a22: { a23: { a24: { a25: { a26: 1 } } } } } } } } } } } } } } } } } } } } } } } } } };
+
+        const result: {}[] = [];
+
+        const handler = (newValue: any, oldValue: any, host: any, property: string) =>
+        {
+            console.log(`host`, host);
+            assert.deepEqual(host, a);
+            result.push(`transform changed: ${property} = ${JSON.stringify(oldValue)} -> ${JSON.stringify(newValue)}`);
+        }
+
+        watcher.watchobject(a, a, handler);
+
+        const a2 = a.a1.a2;
+
+        result.length = 0;
+        a.a1.a2 = null as any;
+
+        assert.deepEqual(result, [
+            "transform changed: a2.a3.a4.a5.a6.a7.a8.a9.a10.a11.a12.a13.a14.a15.a16.a17.a18.a19.a20.a21.a22.a23.a24.a25.a26 = 1 -> undefined",
+        ]);
+
+        result.length = 0;
+        a.a1.a2 = a2;
+
+        assert.deepEqual(result, [
+            "transform changed: a2.a3.a4.a5.a6.a7.a8.a9.a10.a11.a12.a13.a14.a15.a16.a17.a18.a19.a20.a21.a22.a23.a24.a25.a26 = undefined -> 1",
+        ])
+
+    });
+
+    it('测试监听子对象被赋值为null是否正常', () =>
+    {
+        const result: {}[] = [];
+
+        const handler = (newValue: any, oldValue: any, host: any, property: string) =>
+        {
+            result.push(`transform changed: ${property} = ${JSON.stringify(oldValue)} -> ${JSON.stringify(newValue)}`);
+        }
+
+        const material = {
+            shadername: 'shader',
+            defines: {
+                'FOO': 1,
+                'BAR': 2,
+            },
+        }
+
+        watcher.watchobject(material, {
+            shadername: 'shader',
+            defines: {
+                'FOO': 1,
+                'BAR': 2,
+            },
+        }, handler);
+
+        const handler1 = (newValue: any, oldValue: any, host: any, property: string) =>
+        {
+            result.push(`transform changed: ${property} = ${JSON.stringify(oldValue)} -> ${JSON.stringify(newValue)}`);
+        }
+        watcher.watchobject(material, {
+            shadername: 'shader',
+            defines: {
+                'FOO': 1,
+                'BAR': 2,
+            },
+        }, handler1);
+
+        result.length = 0;
+        material.defines = null as any;
+        assert.deepEqual(result, [
+            `transform changed: FOO = 1 -> undefined`,
+            `transform changed: BAR = 2 -> undefined`,
+            `transform changed: FOO = 1 -> undefined`,
+            `transform changed: BAR = 2 -> undefined`,
+        ]);
+
+        result.length = 0;
+        material.defines = {
+            'FOO': 1,
+            'BAR': 2,
+        };
+
+        assert.deepEqual(result, [
+            "transform changed: FOO = undefined -> 1",
+            "transform changed: BAR = undefined -> 2",
+            "transform changed: FOO = undefined -> 1",
+            "transform changed: BAR = undefined -> 2",
+        ]);
+
+        watcher.unwatchobject(material, {
+            shadername: 'shader',
+            defines: {
+                'FOO': 1,
+                'BAR': 2,
+            },
+        }, handler);
+
+        result.length = 0;
+        material.defines = null as any;
+        assert.deepEqual(result, [
+            "transform changed: FOO = 1 -> undefined",
+            "transform changed: BAR = 2 -> undefined",
+        ]);
+
+        result.length = 0;
+        material.defines = {
+            'FOO': 1,
+            'BAR': 2,
+        };
+        assert.deepEqual(result, [
+            "transform changed: FOO = undefined -> 1",
+            "transform changed: BAR = undefined -> 2",
+        ]);
+    });
+
     it('使用`watcher.watchobject`监听transform', () =>
     {
         // 变换
